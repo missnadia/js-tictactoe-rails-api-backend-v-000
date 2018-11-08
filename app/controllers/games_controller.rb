06929c557713126c685d2ab685a93f1ca86ef26e
Nadia Lee <miss.nadia.lee@gmail.com>
2018-11-08 20:22:45 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,8 +2,8 @@ class GamesController < ApplicationController
   before_action :set_game, only: [:show, :update]
 
   def index
-    @games = Game.all
-    render json: @games
+    games = Game.all
+    render json: games
   end
 
   def show
@@ -11,8 +11,8 @@ class GamesController < ApplicationController
   end
 
   def create
-    @game = Game.create(game_params)
-    render json: @game, status: 201
+    game = Game.create(game_params)
+    render json: game, status: 201
   end
 
   def update
